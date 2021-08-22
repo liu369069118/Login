b7f882941ad9c30901d2e098090665493aaf18c0
liutao.0702 <liutao.0702@bytedance.com>
2021-08-22 14:32:10 +0800

    feat:Login binary

--- Example/Login/ViewController.swift
+++ Example/Login/ViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+import Login
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        LoginManager().login()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 }
---
+++ Login/Classes/LoginManager.swift
@@ -0,0 +1,14 @@
+//
+//  LoginManager.swift
+//  Login
+//
+//  Created by liutao on 2021/8/22.
+//
+
+import UIKit
+
+open class LoginManager: NSObject {
+    open func login() {
+        print("login success!")
+    }
+}
